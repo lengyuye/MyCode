725df31d0e2bc8747477c8362f21d07d8bbc0c55
lengyuye <lengyuye171@163.com>
2015-03-14 18:27:19 +0800

    增加了 按钮 跳的功能

---
+++ BothRun2/Assets/Scripts/Monkey.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monkey : MonoBehaviour {
+    Transform m_transform;
+    float delayTime = 0.5f;
+	// Use this for initialization
+	void Start () {
+        m_transform = this.transform;
+	}
+	
+
+    void Jump()
+    {
+        Debug.Log("Jump");
+        m_transform.Translate(Vector3.up * 0.5f); 
+    }
+
+    IEnumerator Down()
+    {
+        yield return new WaitForSeconds(0.5f);
+        m_transform.Translate(Vector3.down * 0.5f); 
+    }
+}
